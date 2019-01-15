065eec0d233cd99f9a194beaaf2be5f9dc54cb28
Matt Benson <mattb@signupgenius.com>
2019-01-15 14:02:37 -0500

    Initial checkin
    Remember to make the repo online first, pull it down, THEN it will see if new files are added.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lorem Ipsdumb
+Profoundly dumb placeholder text placed into various CSS Grid layouts. 
+
+# What This Is
+This is a randomized quote website to serve up the bonkers stuff my co-worker says. The website uses React and a smidge of other scripts to load the content and layouts.
+
+# Plugins & Tech
+- Scripting: Reach & vanilla JavaScript
+- CSS: Mostly custom, uses Bourbon Neat for the responsive structure, also Normalize for general misc.
+- Quotes: 100% crazy talk from the guy at work. Seriously can't make this stuff up.
+- Build process: I'm using PrePros for the build process. Small site, so no need to Gulp this.
---
+++ js/app.js
@@ -0,0 +1,80 @@
+/* ************************************************************************ */
+// Libs to load
+/* ************************************************************************ */
+// Prepended scripts like libraries and misc
+//@prepros-prepend 
+
+// Appended scripts like components and other functions
+//@prepros-append libs/like_button.js
+
+//console.log("JavaScript libs loaded and ready");
+
+
+/* ************************************************************************ */
+// Init
+/* ************************************************************************ */
+if (typeof mcbrusselsprouts == 'undefined') {
+    var mcbrusselsprouts = {};
+}
+
+
+/* ************************************************************************ */
+// 1. Establish Main class
+/* ************************************************************************ */
+mcbrusselsprouts = {
+    tests: {
+        //isUsingRetina: window.devicePixelRatio > 1,
+        //isTouchDevice: Modernizr.touch,
+        //isMobileDevice: $.browser.mobile
+    },
+    app: (function() {
+        return {
+            init: function() {
+                this.initScripts();
+                this.registerEvents();
+            },
+
+            initScripts: function() {
+                // 1. Get the quote data from json
+                // 2. Determine the quote to display & update url
+                // 3. Count up the active indicators and set quote navigation
+                // 4.
+                //mcbrusselsprouts.sproutdata.init();
+                //mcbrusselsprouts.setsprout.init();
+                //mcbrusselsprouts.google.init();
+                //mcbrusselsprouts.sproutpager.init();
+                //mcbrusselsprouts.shareTwitter.init();
+                //mcbrusselsprouts.shareInstagram.init();
+                //mcbrusselsprouts.shareFacebook.init();
+
+                // Modal text
+                // Set color scheme with fades and other animations
+                //
+            },
+
+            registerEvents: function() {
+
+                // Stop the hash, man
+                /*
+                $(document).on("click", "a[href='#']", function(e) {
+                    e.preventDefault();
+                });
+                */
+
+            }
+        };
+    })()
+};
+
+
+/* ************************************************************************ */
+// Press RUN button on your TG16 controller
+/* ************************************************************************ */
+//$(function() {
+    console.log("Lorem Ipsdumb app ready");
+    //mcbrusselsprouts.app.init();
+//});
+
+//$(document).ready(function($) {
+    //
+//});
